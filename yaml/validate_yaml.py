58d72b320b55dd412355419a86106499f3058596
unknown <skbofac.812@gmail.com>
2023-11-23 19:24:08 +0530

    YAML

--- yaml/validate_yaml.py
+++ yaml/validate_yaml.py
@@ -8,3 +8,13 @@ with open('YAML.yml', 'r') as file:
         print(yaml.safe_load(file))
     except yaml.YAMLError as exc:
         print(exc)
+
+
+# OUTPUT
+"""
+{'name': 'Shiva', 'roll.no': 48, 
+'server': {'name': 'server name', 'capacity': '50GB'}, 
+'social_media': ['instagram', 'fb', 'x (former twitter)', 'YT'], 
+'testing': 'this is\nmultiline\nstring\n', 
+'list_of_servers': [{'server_1': None, 'name': 'ec2', 'vol': '8GB'}, {'server_2': None, 'name': 'fargate', 'vol': '10GB'}]}
+"""
